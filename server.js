57ebf9c5260d17e74de6480ec488860ec10b6ea6
Amanda Dattalo <Amanda@10-248-12-201.wireless.oregonstate.edu>
2017-11-14 08:35:46 -0800

    testing

--- server.js
+++ server.js
@@ -3,34 +3,36 @@
  */
 var http = require('http');
 var fs = require('fs');
+var html = fs.readFileSync('./public/index.html',"utf8");
+var css = fs.readFileSync('./public/style.css',"utf8");
+var js = fs.readFileSync('./public/index.js',"utf8");
+var jpg = fs.readFileSync('./public/logo.jpg');
+//var htmlERR = fs.readFileSync('./public/404.html',"utf8");
 
 http.createServer(function(req, res){
-	if(req.url == '/index.html' || req.url == '/'){
-		fs.readFile('./public/index.html', function(err, html){					
-			res.writeHeader(200,{"Content-Type": "text/html"});
-			res.write(html);
-			res.end();
-		});
+	if(req.url == '/index.html' || req.url == '/'){	
+		res.writeHeader(200,{"Content-Type": "text/html"});
+		res.write(html);
+		res.end();
 	}
-	else if(req.url == '/style.css'){
-		fs.readFile('./public/style.css', function(err, css){	
-			res.writeHeader(200,{"Content-Type": "text/css"});
-			res.write(css);
-			res.end();
-		});
+	else if(req.url == '/style.css'){	
+		res.writeHeader(200,{"Content-Type": "text/css"});
+		res.write(css);
+		res.end();
 	}
 	else if(req.url == '/index.js'){		
-		fs.readFile('./public/index.js', function(err, js){	
-			res.writeHeader(200,{"Content-Type": "application/json"});
-			res.write(js);
-			res.end();
-		});
+		res.writeHeader(200,{"Content-Type": "application/json"});
+		res.write(js);
+		res.end();
 	}
 	else if(req.url == '/logo.jpg'){
-		fs.readFile('./public/logo.jpg', function(err, jpg){	
-			res.writeHeader(200,{"Content-Type": "picture/jpg"});
-			res.write(jpg);
-			res.end();
-		});		
+		res.writeHeader(200,{"Content-Type": "image/gif"});
+		res.write(jpg);
+		res.end();	
 	}
-}).listen(4200);
+	/*else{
+		res.writeHeader(404,{"Content-Type": "text/html"});
+		res.write(htmlERR);
+		res.end();
+	}*/
+}).listen(3000);
